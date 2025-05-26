235f2e33475821c18d18dab92630df503d5e6582
Erik Ang <137373695+Erik-Ang@users.noreply.github.com>
2025-05-26 00:12:16 -0400

    Added base folder as portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header.jsx";
 import HomePage from "./components/HomePage.jsx";
 import Experience from "./components/Experience.jsx";
 import Workflow from "./components/Workflow.jsx";
